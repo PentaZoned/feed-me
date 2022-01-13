5532d3abe4dc20db9c63e11ccbfc5f4f3848c56f
plotinusspascual <plotinusspascual@gmail.com>
2022-01-13 11:20:06 -0800

    changed search autocomplete filter to just Cities and added button event listener to get cities

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#autocomplete');
 let autocomplete;
+
 function initAutocomplete(){
   autocomplete = new google.maps.places.Autocomplete(
     document.getElementById('autocomplete'),
     {
-      types: ['establishment'],
-      componentRestrictions: {'country': ['US']},
-      fields: ['place_id', 'geometry', 'name']
+      types: ['(cities)'],
+      componentRestrictions: {'country': ['us']}
     });
 }
+
+
+var formSubmitHandler = function(event){
+  event.preventDefault();
+  var city = searchInput.value.trim();
+  console.log(city);
+}
+searchForm.addEventListener('submit', formSubmitHandler);
