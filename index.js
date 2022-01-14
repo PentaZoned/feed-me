9aca747ee45b8e4074a9649213d6218695c1ec53
plotinusspascual <plotinusspascual@gmail.com>
2022-01-13 22:47:57 -0800

    successfully fetched yelp api

--- index.js
+++ index.js
@@ -23,28 +23,25 @@ var formSubmitHandler = function(event){
 }
 searchForm.addEventListener('submit', formSubmitHandler);
 
-var API_BASE_URL = 'https://api.yelp.com/v3';
-var BEARER_TOKEN = 'uqPekTdjMxPwfPByRjaRhuSxoWXztbJfGo6_yHs6utX8o3e5WZPCxQM1DxsjrO-XhEj2sNaG7HMrxnhGvRihWa5iQI7mXvRlOM-w_XRXd3UxOMswA9Bxp_jIFBB-YHYx';
-//https://api.yelp.com/v3/businesses/search?term=burger&location=portland
+
+let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 
 function getYelpApi(test){
-  jQuery.ajaxPrefilter(function (options) {
-    if (options.crossDomain && jQuery.support.cors) {
-      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-  });
-
-  $.ajax({
-    url: 'https://api.yelp.com/v3/business/search?term='
-      + 'burger' 
-      + '&location='
-      + test,
-    method: 'GET',
-    headers: {
-      authorization: '${BEARER_TOKEN}'
-    }
-  }).then(function(response){
-    console.log(response)
-  })
-
+var token = 'Bearer uqPekTdjMxPwfPByRjaRhuSxoWXztbJfGo6_yHs6utX8o3e5WZPCxQM1DxsjrO-XhEj2sNaG7HMrxnhGvRihWa5iQI7mXvRlOM-w_XRXd3UxOMswA9Bxp_jIFBB-YHYx'
+    var yelp_search_url = 'https://api.yelp.com/v3/businesses/search'
+    var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com'
+      var requestObj = {
+        'url': cors_anywhere_url + '/' + yelp_search_url,
+        'data': {term: 'restaurants', location: test},
+        headers: {'Authorization': token},
+        error: function(jqXHR, textStatus, errorThrown){
+          console.log('AJAX error, jqXRH = ', jqXHR, ', textStatus =',
+          textStatus, ', errorThrown = ', errorThrown)
+        }
+      }
+      $.ajax(requestObj)
+        .done(function(response){
+          console.log('typeof response =' + typeof response)
+          console.log('response = ', response)
+        })
 }
