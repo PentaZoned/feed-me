f4247578ddc9053ab7a282a0d2b25253c4b6a662
plotinusspascual <plotinusspascual@gmail.com>
2022-01-14 11:08:18 -0800

    got values of checkboxes to work with yelp api as data parameters for search

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var formSubmitHandler = function(event){
   citySplit = city.split(',');
   console.log(citySplit[0]);
   cityName = citySplit[0];
-  getYelpApi(cityName);
+  
 
   var checkedE1 = $('input:checked');
   var selected = [];
@@ -27,19 +27,25 @@ var formSubmitHandler = function(event){
     selected.push($(this).val());
   });
   console.log('Budget is :', selected.join(','));
+  var budget = selected.join(',');
+  getYelpApi(cityName, budget);
 }
 searchForm.addEventListener('submit', formSubmitHandler);
 
 
 //let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 
-function getYelpApi(cityName){
+function getYelpApi(cityName, budget){
 var token = 'Bearer uqPekTdjMxPwfPByRjaRhuSxoWXztbJfGo6_yHs6utX8o3e5WZPCxQM1DxsjrO-XhEj2sNaG7HMrxnhGvRihWa5iQI7mXvRlOM-w_XRXd3UxOMswA9Bxp_jIFBB-YHYx'
     var yelp_search_url = 'https://api.yelp.com/v3/businesses/search'
     var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com'
       var requestObj = {
         'url': cors_anywhere_url + '/' + yelp_search_url,
-        'data': {term: 'restaurants', location: cityName},
+        'data': {
+                term: 'restaurants',
+                location: cityName,
+                price: budget
+              },
         headers: {'Authorization': token},
         error: function(jqXHR, textStatus, errorThrown){
           console.log('AJAX error, jqXRH = ', jqXHR, ', textStatus =',
